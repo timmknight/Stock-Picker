fa9d3a6834705338c4a29cd6cdf6b01c6ed21172
Tim Knight <timmilesknight@gmail.com>
2015-06-20 18:21:37 +0100

    Clean up tabs

--- stockpicker.rb
+++ stockpicker.rb
@@ -10,27 +10,32 @@ def stock_picker(stock_prices)
 
 # Loop through the stock array and get the sell stock value
 	stock_prices.each_with_index do |sellValue, sellStockIndex|
-			sell_day = sellStockIndex
+		sell_day = sellStockIndex
 
 
 # With the stock value loop through the array getting the buy values
-			stock_prices.each_with_index do |buyValue, buyStockIndex|
+	stock_prices.each_with_index do |buyValue, buyStockIndex|
 
-				buy_day = buyStockIndex
-				# Work out if the sell stock value is greater than the buy stock value 
-				if buy_day < sell_day && (sellValue - buyValue >=0)
-					# assign the result of sell stocks value - buy stocks value
-					 minProfit = sellValue - buyValue
+		buy_day = buyStockIndex
 
-					 # if this itereations profit is larger than the last assign it to max value and add to the array
-					if minProfit > maxProfit
-						maxProfit = minProfit
-						# Push that value into the array which will have all the profitable stock purchases in
-						arr.push([buy_day, sell_day, maxProfit])
-					end
-				end	
+		# Work out if the sell stock value is greater than the buy stock value 
+		if buy_day < sell_day && (sellValue - buyValue >=0)
+
+			# assign the result of sell stocks value - buy stocks value
+			 minProfit = sellValue - buyValue
+
+			 # if this itereations profit is larger than the last assign it to max value and add to the array
+			if minProfit > maxProfit
+				maxProfit = minProfit
+
+				# Push that value into the array which will have all the profitable stock purchases in
+				arr.push([buy_day, sell_day, maxProfit])
+
 			end
+		end	
 	end
+end
+
 # Get the last stock info from the array - this will be the highest profit stock and put values to screen
 	puts "Bought on index #{arr.last[0]} " 
 	puts "Sold on index #{arr.last[1]}"
